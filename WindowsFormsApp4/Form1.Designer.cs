1bdcf70dea720349af9f02560ee16e571c1c5082
guminskaya484@gmail.com <guminskaya484@gmail.com>
2024-10-16 15:10:41 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private PictureBox pictureBox;
+        private Button chooseImageButton;
+        private TrackBar zoomTrackBar;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.chooseImageButton = new System.Windows.Forms.Button();
+            this.zoomTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zoomTrackBar)).BeginInit();
+            this.SuspendLayout();
+
+            // pictureBox
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+
+            // chooseImageButton
+            this.chooseImageButton.Location = new System.Drawing.Point(12, 420);
+            this.chooseImageButton.Name = "chooseImageButton";
+            this.chooseImageButton.Size = new System.Drawing.Size(150, 30);
+            this.chooseImageButton.TabIndex = 1;
+            this.chooseImageButton.Text = "Выбрать изображение";
+            this.chooseImageButton.UseVisualStyleBackColor = true;
+            this.chooseImageButton.Click += new System.EventHandler(this.chooseImageButton_Click);
+
+            // zoomTrackBar
+            this.zoomTrackBar.Location = new System.Drawing.Point(180, 420);
+            this.zoomTrackBar.Minimum = 1;
+            this.zoomTrackBar.Maximum = 20; // Масштабируем от 1 (10%) до 20 (200%)
+            this.zoomTrackBar.Name = "zoomTrackBar";
+            this.zoomTrackBar.Size = new System.Drawing.Size(300, 56);
+            this.zoomTrackBar.TabIndex = 2;
+            this.zoomTrackBar.Value = 10; // По умолчанию 100%
+            this.zoomTrackBar.Scroll += new System.EventHandler(this.zoomTrackBar_Scroll);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(630, 480);
+            this.Controls.Add(this.zoomTrackBar);
+            this.Controls.Add(this.chooseImageButton);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Просмотр изображений";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zoomTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private Image originalImage; // Переменная для хранения оригинального изображения
+        private float zoomFactor = 1.0f; // Коэффициент масштабирования
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Метод для выбора изображения
+        private void chooseImageButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Загружаем изображение
+                    originalImage = Image.FromFile(openFileDialog.FileName);
+                    pictureBox.Image = originalImage;
+                    pictureBox.SizeMode = PictureBoxSizeMode.Zoom; // Устанавливаем режим отображения изображения
+                    zoomFactor = 1.0f; // Сбрасываем коэффициент масштабирования
+                    pictureBox.Refresh();
+                }
+            }
+        }
+
+        // Метод для масштабирования изображения
+        private void zoomTrackBar_Scroll(object sender, EventArgs e)
+        {
+            if (originalImage != null)
+            {
+                // Получаем значение масштаба с ползунка TrackBar (значение от 1 до 200%)
+                zoomFactor = zoomTrackBar.Value / 10.0f;
+
+                // Обновляем размеры PictureBox относительно коэффициента масштабирования
+                pictureBox.Width = (int)(originalImage.Width * zoomFactor);
+                pictureBox.Height = (int)(originalImage.Height * zoomFactor);
+
+                // Центрируем PictureBox
+                pictureBox.Left = (this.ClientSize.Width - pictureBox.Width) / 2;
+                pictureBox.Top = (this.ClientSize.Height - pictureBox.Height - 60) / 2; // -60 для учета кнопок
+            }
+        }
+    }
+}
